5080ba21cd5f02fdc717af322a7c88935bb540b5
Nancy-1311 <nancykhundia44@gmail.com>
2026-04-28 23:56:10 +0530

    Update adminController.js

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -304,3 +304,37 @@ export const deleteReviewAdmin = async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 };
+
+export const createUserByAdmin = async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+
+    if (!name || !email || !password || !role) {
+      return res.status(400).json({
+        message: "All fields are required",
+      });
+    }
+
+    const exists = await User.findOne({ email });
+
+    if (exists) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      role,
+    });
+
+    res.status(201).json(user);
+
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};

145f52e205404220991fdc7b736f523c7aa1d073
Nancy-1311 <nancykhundia44@gmail.com>
2026-05-01 06:32:20 +0530

    Update paymentController.js

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -30,7 +30,7 @@ export const createCheckoutSession = async (req, res) => {
       ],
      success_url: `https://lms-frontend-puce-gamma.vercel.app/success?bookingId=${bookingId}&session_id={CHECKOUT_SESSION_ID}`,
       // cancel_url: "https://lms-frontend-puce-gamma.vercel.app/cancel",
-       cancel_url: "http://localhost:5173/cancel",
+     cancel_url: `https://lms-frontend-puce-gamma.vercel.app/tutors/${tutorId}`
     });
 
     res.json({ url: session.url });
